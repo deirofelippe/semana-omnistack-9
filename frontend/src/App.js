f67c0d5828a6109be8b5f199bc2e42b59a2ba041
felippedeiro <sergiofelippe.deiro@gmail.com>
2019-10-03 17:58:24 -0300

    cria rotas e organiza as pastas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,16 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 import logo from './assets/logo.svg';
+import Routes from './routes';
 
-/*
-. componente é um conjunto de codigo q pode ser reutilizado (menu, cabeçalho)
-. informacao guardada dentro do componente, fica no estado do componente. uma listagem de pessoas q sera armazenada em um lugar p ser mostrada depois em tela.
-*/
+
 function App() {
-  /*a cada mudanca de valor no input, o email é atualizado pelo setEmail*/
-  /*cria a componentizacao e o estado da Session do User */
-  const [email, setEmail] = useState('');
-
-  async function handleSubmit(event){
-    event.preventDefault(); /*n permite q o submit faça sua acao padrao, q é atualizar a pagina*/
-
-    const response = await api.post('/sessions', { email });
-    const {_id} = response.data;
-    
-    localStorage.setItem("user", _id);
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-          type="email" 
-          id="email" 
-          placeholder="Seu melhor e-mail"
-          value={email}
-          onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
       </div>
     </div>
   );
---
+++ frontend/src/pages/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React, {useEffect} from 'react';
+
+export default function Dashboard(){
+    /*quando alguma variavel do array sofrer atualizacoes, a funcao ira ser executada */
+    useEffect(() => {
+        
+    }, []);
+    
+    return <h1>Dashboard</h1>
+}
